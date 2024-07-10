89844bc2c0faddd6410400a51713923b393829f9
Jerry Zhang <jinxuan0920@gmail.com>
2024-07-09 21:33:55 -0400

    updated favicon and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
-import { GeistSans } from "geist/font/sans";
+import '@fontsource/nunito/400.css';
+import '@fontsource/nunito/700.css';
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Electrium Shop",
-  description: "Electrium Mobility's Shop",
+    metadataBase: new URL(defaultUrl),
+    title: "Electrium Shop",
+    description: "Electrium Mobility's Shop",
+    icons: {
+        icon: '/img/favicon.png',
+    },
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={GeistSans.className}>
-      <body>
-          {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body style={{ fontFamily: 'Nunito, sans-serif' }}>
+        {children}
+        </body>
+        </html>
+    );
 }
--- components/shop/Navbar.tsx
+++ components/shop/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="flex justify-between items-center p-4 bg-white">
             <div className="flex items-center">
-                <Image src="/img/logo.png" alt="Electrium Mobility" width={40} height={40} />
+                <Image src="/img/favicon.png" alt="Electrium Mobility" width={40} height={40} />
                 <span className="ml-2 text-xl font-semibold">electrium mobility</span>
             </div>
             <div className="space-x-4">
@@ -13,7 +13,7 @@ export default function Navbar() {
                 <Link href="https://electriummobility.com/team" className="text-gray-600 hover:text-gray-900">Team</Link>
                 <Link href="https://electriummobility.com/docs/W2024-projects/project1_2023" className="text-gray-600 hover:text-gray-900">Projects</Link>
                 <Link href="https://electriummobility.com/sponsors" className="text-gray-600 hover:text-gray-900">Sponsors</Link>
-                <Link href="/shop" className="text-gray-600 hover:text-gray-900">Shop</Link>
+                <Link href="" className="text-gray-600 hover:text-gray-900">Shop</Link>
                 <Link href="https://electriummobility.com/contact" className="text-gray-600 hover:text-gray-900">Contact</Link>
             </div>
             <div className="flex items-center space-x-4">
