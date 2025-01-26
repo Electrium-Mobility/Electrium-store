65021d9468b335afe990e2b41d0ccf850a076fa0
Vedant Grover <vedantvgrover@gmail.com>
2025-01-26 11:43:59 -0500

    Creates signup function

--- app/action/auth.tsx
+++ app/action/auth.tsx
@@ -1,4 +1,7 @@
+import { createClient } from "@/utils/supabase/client";
 import { SignUpSchema } from "../lib/definitions";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function signup(formData: FormData) {
   const validatedFields = SignUpSchema.safeParse({
@@ -14,4 +17,19 @@ export async function signup(formData: FormData) {
   }
 
   // TODO: Implement signup functionality
+  const supabase = await createClient();
+
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signUp(data);
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
 }
