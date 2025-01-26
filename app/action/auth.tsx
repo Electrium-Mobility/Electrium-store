106c6ca9522b3094184ac550d0cb0211957b15d9
Vedant Grover <vedantvgrover@gmail.com>
2025-01-25 23:34:50 -0500

    Adds email/password validation

---
+++ app/action/auth.tsx
@@ -0,0 +1,17 @@
+import { SignUpSchema } from "../lib/definitions";
+
+export async function signup(formData: FormData) {
+  const validatedFields = SignUpSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  // TODO: Implement signup functionality
+}
