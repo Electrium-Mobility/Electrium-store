058d0e1e15ea2377539d53dc9a22af0071626ebd
Vedant Grover <vedantvgrover@gmail.com>
2025-01-30 18:28:29 -0500

    Adds name as sign up fields

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,12 +3,12 @@ import { login, signup } from "../action/auth";
 
 export default function LoginPage() {
   return (
-    <form>
+    <form className="h-screen w-full flex flex-col items-center justify-center" action={login}>
       <label htmlFor="email">Email:</label>
       <input id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
       <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
+      <button type="submit">Log in</button>
       <Link href="/signup">Sign up</Link>
     </form>
   )
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import { signup } from "../action/auth";
 
 export default function SignupPage() {
-    return (
-        <form action={signup}>
-            <p>Sign Up</p>
-        </form>
-    )
+  return (
+    <form action={signup} className="h-screen w-full flex flex-col items-center justify-center">
+      <label htmlFor="first_name">First Name:</label>
+      <input id="first_name" name="first_name" type="text" required />
+      <label htmlFor="last_name">Last Name:</label>
+      <input id="last_name" name="last_name" type="text" required />
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button type="submit">Sign up</button>
+      <Link href="/login">Go back to login</Link>
+    </form>
+  );
 }
