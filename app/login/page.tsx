6b75b36876d66c1807065851a4f5f9bdf94c668b
GavinPinto <gavinpinto620@gmail.com>
2025-02-02 01:09:49 -0500

    display error message for invalid login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,18 +7,29 @@ import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 
 export default function LoginPage() {
+  const [displayEmail, setDisplayEmail] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [unsuccessfulLogin, setUnsuccessfulLogin] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  async function handleSubmit(formData: FormData): Promise<void> {
+    if (await login(formData)) { 
+      console.log("Logged in"); 
+    } else {
+      setUnsuccessfulLogin(true); // Display "invalid email or password" message
+    }
+  }
+
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <form className="border border-gray-200 bg-gray-100 p-8 rounded-lg w-full max-w-sm" action={login}>
+      <form className="border border-gray-200 bg-gray-100 p-8 rounded-lg w-full max-w-sm" action={handleSubmit}>
         <h2 className="text-2xl font-bold mb-6 text-green-700">Login</h2>
+        {unsuccessfulLogin && <p className="text-red-600 mb-3 mt-[-15px]">Invalid email or password</p>}
         <label htmlFor="email" className="block text-black mb-2">Email:</label>
-        <input id="email" name="email" type="email" required className="w-full p-2 mb-4 border border-green-300 rounded" />
+        <input id="email" name="email" type="email" value={displayEmail} onChange={(e) => setDisplayEmail(e.target.value)} required className="w-full p-2 mb-4 border border-green-300 rounded" />
         <label htmlFor="password" className="block text-black mb-2">Password:</label>
         <div className="relative">
           <input
