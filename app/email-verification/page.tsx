f9dfcddf695d5ae2d2a0a5fdc0d1b0b1e3d97e30
Vedant Grover <vedantvgrover@gmail.com>
2025-02-02 22:35:22 -0500

    Creates simple email verification page

---
+++ app/email-verification/page.tsx
@@ -0,0 +1,10 @@
+const EmailVerification = () => {
+    return (
+        <div>
+            <h1>Email Verification</h1>
+            <p>Please check your email to verify your account and complete the signup process!</p>
+        </div>
+    )
+}
+
+export default EmailVerification;
