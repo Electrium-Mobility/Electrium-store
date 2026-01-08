e728c008210096d0a131f984081bb8d494f85caf
Ishan <204780804+Ish45883@users.noreply.github.com>
2026-01-08 15:11:38 -0500

    Resolved dynamic rendering check fail

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
+export const dynamic = "force-dynamic";
+
 let Stripe: any = null;
 let stripe: any = null;
 
